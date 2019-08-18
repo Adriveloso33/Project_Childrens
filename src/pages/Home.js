94b549f78159ca09cdb042e2dd2b22c0e242ea47
Adri <d>
2019-08-18 19:46:39 +0200

    Created ValidatedLoginForm Component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,37 +1,38 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import ValidatedLoginForm from './ValidatedLoginForm';
 
-import './styles/Home.css';
-import platziconfLogoImage from '../images/platziconf-logo.svg';
-import astronautsImage from '../images/astronauts.svg';
+import './styles/styles.css';
+//import platziconfLogoImage from '../images/platziconf-logo.svg';
+// import astronautsImage from '../images/astronauts.svg';
 
 export default class Home extends Component {
   render() {
     return (
-      <div className="Home">
-        <div className="container">
-          <div className="row">
-            <div className="Home__col col-12 col-md-4">
-              <img
+      <div className="App">
+        <div className ="App">
+        <br />
+        <h1>Validated Login Form</h1> 
+              {/* <img
                 src={platziconfLogoImage}
                 alt="Platzi Conf Logo"
                 className="img-fluid mb-2"
-              />
+              /> */}
 
-              <h1>Badge Management System</h1>
+              <br />
+              <ValidatedLoginForm />
+              <br />
               <Link className="btn btn-primary" to="/badges">
                 Start
               </Link>
-            </div>
-
             <div className="Home__col d-none d-md-block col-md-8">
-              <img
+              {/* <img
                 src={astronautsImage}
                 alt="Astronauts"
                 className="img-fluid p-4"
-              />
+              /> */}
             </div>
-          </div>
+          
         </div>
       </div>
     );
